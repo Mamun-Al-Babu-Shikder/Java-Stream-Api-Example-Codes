f0c95c26225663c6777ae021e39ada6e21bc8be0
mamun <aalmamun295@gmail.com>
2021-01-21 23:52:44 +0600

    Different way to create a java stream

--- src/com/mcubes/CreateStreams.java
+++ src/com/mcubes/CreateStreams.java
@@ -2,6 +2,9 @@ package com.mcubes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -42,7 +45,33 @@ public class CreateStreams {
         Stream<Integer> stream3 = list.stream();
         stream3.forEach(System.out::println); // print each element of the stream
 
+        /**
+         * Stream.generate()
+         * creating a stream from generated elements.
+         * This will produce a stream of 10 random numbers.
+         * We have restricted the elements count using limit() function.
+         */
+        System.out.println("\nStream.generate():");
+        Stream<Integer> stream4 = Stream.generate(()->new Random().nextInt(10));
+        stream4.limit(10).forEach(System.out::println); // print each element of the stream
 
+        /**
+         * Stream of String chars
+         * creating a stream from the characters of a given string.
+         * "String".chars() method return an IntStream.
+         */
+        System.out.println("\nStream of String chars:");
+        IntStream stream5 = "Hello_World".chars();
+        stream5.forEach(System.out::println); // print each element of the stream. every element an int value
+
+        /**
+         * Stream tokens
+         * creating the stream of tokens received from splitting from a string.
+         */
+        System.out.println("\nStream tokens:");
+        String s = "White,Black,Red,Green,Gray,Orange";
+        Stream<String> stream6 = Stream.of(s.split(","));
+        stream6.forEach(System.out::println);
 
     }
 }
